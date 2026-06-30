fdd49933e3c7d179de52c872e7c2ffeb4c973834
intervalplace <julnerapple@hotmail.com>
2026-06-30 22:51:01 +0200

    Add files via upload

--- src/validators/order.ts
+++ src/validators/order.ts
@@ -1,22 +1,6 @@
 import type { AonObject } from "@intervalplace/aon-sdk";
 import { verifyObjectSignature } from "./signatures.js";
-import {
-  requireNamespace,
-  requireReferenceTypes,
-  type ValidationGraph,
-} from "./graph.js";
 
 export async function validateOrder(obj: AonObject) {
   await verifyObjectSignature(obj);
-}
-
-export async function validateOrderGraph(
-  obj: AonObject,
-  graph: ValidationGraph
-) {
-  await validateOrder(obj);
-
-  const [auth] = requireReferenceTypes(graph, obj, ["authorization"]);
-
-  requireNamespace(auth.namespace, obj.namespace);
 }
